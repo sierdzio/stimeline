11ed12ba5b8264d8a5f0da8f8243d684660a1de2
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2017-04-22 10:53:58 +0200

    Expand SObject unit test.

--- tests/auto/SObjectTest/SObjectTest.cpp
+++ tests/auto/SObjectTest/SObjectTest.cpp
@@ -1,8 +1,12 @@
 #include <QtTest>
 #include <QCoreApplication>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QFile>
 
 #include "sobject.h"
 #include "sdatetime.h"
+#include "tags.h"
 
 class SObjectTest : public QObject
 {
@@ -16,6 +20,8 @@ private slots:
     void initTestCase();
     void cleanupTestCase();
     void testDefaults();
+    void testConversions();
+    void testJson();
 };
 
 SObjectTest::SObjectTest()
@@ -37,13 +43,58 @@ void SObjectTest::cleanupTestCase()
 void SObjectTest::testDefaults()
 {
     SObject object;
-    Q_ASSERT(!object.mId.isEmpty());
-    Q_ASSERT(object.mType == SObject::ObjectType::None);
-    Q_ASSERT(object.mName.isEmpty());
-    Q_ASSERT(object.mPicturePath.isEmpty());
-    Q_ASSERT(object.mDescription.isEmpty());
-    Q_ASSERT(object.mFrom.toString() == SDateTime().toString());
-    Q_ASSERT(object.mTo.toString() == SDateTime().toString());
+    QVERIFY(!object.mId.isEmpty());
+    QVERIFY(object.mType == SObject::ObjectType::None);
+    QVERIFY(object.mName.isEmpty());
+    QVERIFY(object.mPicturePath.isEmpty());
+    QVERIFY(object.mDescription.isEmpty());
+    QVERIFY(object.mFrom.toString() == SDateTime().toString());
+    QVERIFY(object.mTo.toString() == SDateTime().toString());
+    QVERIFY(object.isValid());
+
+    QJsonObject jobj = object.toJson();
+    QVERIFY(jobj.value(Tags::id).toString().toLatin1() == object.mId);
+    QVERIFY(jobj.value(Tags::type).toString() == SObject::typeToString(SObject::ObjectType::None));
+    QVERIFY(jobj.value(Tags::name).toString().isEmpty());
+    QVERIFY(jobj.value(Tags::picturePath).toString().isEmpty());
+    QVERIFY(jobj.value(Tags::description).toString().isEmpty());
+    QVERIFY(jobj.value(Tags::from).toString() == SDateTime().toString());
+    QVERIFY(jobj.value(Tags::to).toString() == SDateTime().toString());
+}
+
+void SObjectTest::testConversions()
+{
+    QVERIFY(SObject::typeToString("0") == "None");
+    QVERIFY(SObject::typeToString("5") == "Map");
+
+    QVERIFY(SObject::typeToString(0) == "None");
+    QVERIFY(SObject::typeToString(5) == "Map");
+
+    QVERIFY(SObject::typeToString(SObject::ObjectType::None) == "None");
+    QVERIFY(SObject::typeToString(SObject::ObjectType::Map) == "Map");
+
+    QVERIFY(SObject::stringToType("None") == SObject::ObjectType::None);
+    QVERIFY(SObject::stringToType("Map") == SObject::ObjectType::Map);
+}
+
+void SObjectTest::testJson()
+{
+    QFile file(":/testData/sobject1.json");
+    Q_ASSERT(file.open(QFile::Text | QFile::ReadOnly));
+
+    QJsonParseError err;
+    const QJsonDocument doc = QJsonDocument::fromJson(file.readAll(), &err);
+    QVERIFY(!doc.isEmpty() && err.error == QJsonParseError::NoError);
+
+    SObject obj(doc.object());
+    QVERIFY(obj.isValid());
+    QVERIFY(obj.mId == "1c462dec69dfc2ada12db5a02e4dceaf04b40ef6");
+    QVERIFY(obj.mType == SObject::ObjectType::Event);
+    QVERIFY(obj.mName == "Event 2");
+    QVERIFY(obj.mPicturePath.isEmpty());
+    QVERIFY(obj.mDescription == "BBB");
+    QVERIFY(obj.mFrom.toString() == "1-1-3 1:1:4");
+    QVERIFY(obj.mTo.toString() == "1-1-3 1:1:7");
 }
 
 QTEST_MAIN(SObjectTest)
--- tests/auto/SObjectTest/SObjectTest.pro
+++ tests/auto/SObjectTest/SObjectTest.pro
@@ -4,9 +4,12 @@ CONFIG += qt warn_on depend_includepath testcase
 
 TEMPLATE = app
 
+include(../../data/testData.pri)
+
 INCDIR = ../../../src
 INCLUDEPATH += $$INCDIR
-HEADERS += $$INCDIR/sobject.h $$INCDIR/sdatetime.h $$INCDIR/sassistant.h
+HEADERS += $$INCDIR/sobject.h $$INCDIR/sdatetime.h $$INCDIR/sassistant.h \
+           $$INCDIR/tags.h
 SOURCES += $$INCDIR/sobject.cpp $$INCDIR/sdatetime.cpp $$INCDIR/sassistant.cpp
 
 SOURCES += SObjectTest.cpp
