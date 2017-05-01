4883d923972ca8fd7882445c4e971722eb68be47
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2017-05-01 12:17:14 +0200

    Begin work on SCalendarTest.

--- tests/auto/SCalendarTest/SCalendarTest.pro
+++ tests/auto/SCalendarTest/SCalendarTest.pro
@@ -5,11 +5,12 @@ CONFIG += qt warn_on depend_includepath testcase
 
 TEMPLATE = app
 
+include(../../data/testData.pri)
 include(../../testConfig.pri)
 
 INCDIR = ../../../src
 INCLUDEPATH += $$INCDIR
-HEADERS += $$INCDIR/sdatetime.h $$INCDIR/scalendar.h
+HEADERS += $$INCDIR/sdatetime.h $$INCDIR/scalendar.h $$INCDIR/tags.h
 SOURCES += $$INCDIR/sdatetime.cpp $$INCDIR/scalendar.cpp
 
 SOURCES += tst_scalendar.cpp
--- tests/auto/SCalendarTest/tst_scalendar.cpp
+++ tests/auto/SCalendarTest/tst_scalendar.cpp
@@ -1,28 +1,43 @@
 #include <QString>
 #include <QtTest>
 #include <QCoreApplication>
+#include <QFile>
+#include <QJsonParseError>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
 
 #include "scalendar.h"
 #include "sdatetime.h"
+#include "tags.h"
 
 class SCalendarTest : public QObject
 {
     Q_OBJECT
 
-public:
-    SCalendarTest();
-
 private Q_SLOTS:
     void testValidity();
+    void testJsonOperators();
 };
-
-SCalendarTest::SCalendarTest()
-{
-}
 
 void SCalendarTest::testValidity()
 {
     SCalendar cal;
+    QVERIFY(cal.isValid());
+}
+
+void SCalendarTest::testJsonOperators()
+{
+    QFile file(":/testData/scalendar1.json");
+    Q_ASSERT(file.open(QFile::Text | QFile::ReadOnly));
+
+    QJsonParseError err;
+    const QJsonDocument doc = QJsonDocument::fromJson(file.readAll(), &err);
+    qDebug() << err.errorString();
+    QVERIFY(!doc.isEmpty() && err.error == QJsonParseError::NoError);
+
+    SCalendar cal;
+    cal.fromJson(doc.object().value(Tags::calendar).toArray());
     QVERIFY(cal.isValid());
 }
 
